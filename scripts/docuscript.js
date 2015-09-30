22062b1ec97d6a93be1eda323d1bf1221170c3b2
Thomas Stoeckert <Thomas@Stoeckert.us>
2015-09-30 15:22:10 -0400

    full framework setup

--- scripts/docuscript.js
+++ scripts/docuscript.js
@@ -1,5 +1,6 @@
 function load_page(name){
 	var contentPane = document.getElementById("content");
 	contentPane.setAttribute("src", "docs\\" + name);
+	console.log("Loaded page" + name);
 	return;
 }
